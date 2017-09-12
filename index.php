62179f39b7487454370ed6bf47a9cce3d1cecaac
Artem <melnykartem10@gmail.com>
2017-09-12 22:34:34 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+// $emailTo="melnykartem10@gmail.com";
+// $subject="Test";
+// $body="Testing mail php function";
+// $headers="From:example@example.com";
+//
+// if(mail($emailTo,$subject,$body,$headers)){
+//   echo "Emal has been sent";
+// }else{
+//   echo "Not sent";
+// }
+
+echo "string;
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form method="get">
+      <input type="text" name="name">
+      <input type="text" name="lastName">
+      <input type="text" name="Age">
+
+      <button type="submit">Confirm</button>
+    </form>
+
+  </body>
+</html>
